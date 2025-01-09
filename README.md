cdf693183f908518e2a20d24874323e4ae549095
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-01-09 23:50:46 +0000

    install script debug version

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
 # Install
 
 ```bash
-git clone https://github.com/scatternoodle/bashrc-exports ${HOME}/.config/bashrc-exports
-echo '
-############ bashrc-exports BEGIN ############
-source_dir=${HOME}/.config/bashrc-exports
-if [ -d "${source_dir}" ]; then
-    for file in "${source_dir}"/*.sh; do
-	[ -r "${file}" ] && source ${file}
-    done
-fi
-############ bashrc-exports END ##############
-' >> ${HOME}/.bashrc
-source ${HOME}/.bashrc
+curl -sS https://github.com/scatternoodle/bashrc-exports/install.sh | bash
 ```
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+install_path="${HOME}"/.config/bashrc-exports
+rc_file="${HOME}"/.bashrc
+rc_script='
+############ bashrc-exports BEGIN ############
+source_dir=${HOME}/.config/bashrc-exports/sources
+if [ -d "${source_dir}" ]; then
+    for file in "${source_dir}"/*.sh; do
+	[ -r "${file}" ] && source "${file}"
+    done
+fi
+############ bashrc-exports END ##############
+'
+
+if [ ! -d "${install_path}" ]; then
+	git clone https://github.com/scatternoodle/bashrc-exports "${install_path}" ||
+		{ echo "Fatal: could not clone repo."; exit 1; }
+fi
+
+if ! grep -q "${rc_script}" "############ bashrc-exports BEGIN ############"; then
+	# echo "${rc_script}" >> "${rc_file}" ||
+		# { echo "Fatal: unable to append to bashrc at ${rc_file}"; exit 1; }
+	echo "fresh bashrc"
+else
+	echo "bashrc already edited"
+fi
+
+# source "${rc_file}"
