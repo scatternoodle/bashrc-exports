a1c5ab8b0cfaa2970d3831c91addcb1b576c899f
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-01-10 01:25:39 +0000

    fix bad variable syntax

--- sources/tmux.sh
+++ sources/tmux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-tmn() { cd "{$1}" && tmux new -s "{$2}" && cd -; }
-tmk () { tmux kill -t "{$1}"; }
+tmn() { cd "${1}" && tmux new -s "${2}" && cd -; }
+tmk () { tmux kill -t "${1}"; }
 alias tmks='tmux kill-server'
 alias tmls='tmux ls'
 alias tma='tmux a'
