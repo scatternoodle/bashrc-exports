be0c266fe5f39f8a675404644ff9d5a3e35b160a
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-01-10 00:54:01 +0000

    remove junk

--- sources/tmux.sh
+++ sources/tmux.sh
@@ -2,6 +2,5 @@
 
 tmn() { cd "{$1}" && tmux new -s "{$2}" && cd -; }
 tmk () { tmux kill -t "{$1}"; }
->>>>>>> 1cd323f (tmux.sh - minor improvements)
 alias tmks='tmux kill-server'
 alias tmls='tmux ls'
