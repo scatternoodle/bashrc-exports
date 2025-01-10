02b70c7b896d9b901c746064286c9fda745e3266
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-01-10 00:53:02 +0000

    fix merge conflict in tmux.sh

--- sources/tmux.sh
+++ sources/tmux.sh
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-# tmn PATH NAME
-# Creates a new tmux session at the given PATH with NAME. Automatically returns
-# to original CWD after creating the session.
-tmn() {
-	cd $1 || return
-	tmux new -s $2 && cd -
-}
-export -f tmn
-
-tmk () { tmux kill -t $1; }
-export -f tmk
-
-=======
 #!/usr/bin/bash
 
 tmn() { cd "{$1}" && tmux new -s "{$2}" && cd -; }
