872d079882ba6ee212053aef2f4cbff1a69e6d0c
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-01-10 00:50:34 +0000

    tmux.sh - minor improvements

--- sources/tmux.sh
+++ sources/tmux.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # tmn PATH NAME
 # Creates a new tmux session at the given PATH with NAME. Automatically returns
 # to original CWD after creating the session.
@@ -10,5 +11,11 @@ export -f tmn
 tmk () { tmux kill -t $1; }
 export -f tmk
 
+=======
+#!/usr/bin/bash
+
+tmn() { cd "{$1}" && tmux new -s "{$2}" && cd -; }
+tmk () { tmux kill -t "{$1}"; }
+>>>>>>> 1cd323f (tmux.sh - minor improvements)
 alias tmks='tmux kill-server'
 alias tmls='tmux ls'
