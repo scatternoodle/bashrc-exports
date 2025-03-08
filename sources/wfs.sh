6b353c874c1565474b211029bc6d155948355f82
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-03-08 22:37:02 +0000

    add wfsint - a command to open a tmux session at the wfs integration script directory, with neovim and a bash shell open

--- sources/wfs.sh
+++ sources/wfs.sh
@@ -1,5 +1,8 @@
 alias twfa='cd "${WFS_ADMIN_CLIENTS_ROOT}" && tmux new -s wfadmin && cd -'
+
 wfa() { find "${WFS_ADMIN_CLIENTS_ROOT}" -type f |
 	fzf --prompt="Select admin client >" |
 	xargs -I {} bash -c 'javaws "{}" & disown'
 }
+
+alias wfsint="tmux new-session -d -s 'wfsint' -c ${HOME}/workspace/github.com/scatternoodle/wfsint/ 'nvim' ';' new-window -d ';' attach"
