cd003f64db2440f80ece9d731a1bbf534a2e9fa6
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-03-08 22:30:08 +0000

    improve tmn - now opens two windows in target directory - 1st with neovim, 2nd just bash

--- sources/tmux.sh
+++ sources/tmux.sh
@@ -3,7 +3,7 @@
 # Other scripts in this repo use tmux incidentally for other purposes, but
 # this script is reserved for commands focused on using/configuring tmux and
 # orchestrating sessions.
-tmn() { cd "${1}" && tmux new -s "${2}" && cd -; }
+tmn() { tmux new-session -d -c "${1}" -s "${2}" 'nvim' ';' new-window -d ';' attach; }
 tmk() { tmux kill-session -t "${1}"; }
 alias tmks='tmux kill-server'
 alias tml='tmux ls'
